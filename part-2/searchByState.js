c481bb2ba5d1eb0a280d34c90a19d496d310f2b1
Joshua Elder <aresx130@gmail.com>
2017-09-12 21:58:11 -0700

    Copied searchByName.js to use as base function for searchByState.js

--- part-2/searchByState.js
+++ part-2/searchByState.js
@@ -1,1 +1,12 @@
 // searchByState.js
+const fs = require('fs')
+
+const data = JSON.parse(fs.readFileSync('clients.json'))
+const name = process.argv[2].toLowerCase()
+const matchingData = []
+console.log(`Finding clients with name "${process.argv[2]}"`)
+data.forEach((rep) => {
+  if (rep.rep_name.toLowerCase().startsWith(name)) matchingData.push({ id: rep.id, rep_name: rep.rep_name })
+})
+
+if (matchingData.length > 0) console.log(matchingData)
