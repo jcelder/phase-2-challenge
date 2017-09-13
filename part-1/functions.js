f7ed2b4a23044a6573d2742fdfbdec9ae7eee140
Joshua Elder <aresx130@gmail.com>
2017-09-12 20:21:30 -0700

    Added final tests and complete function for weekday()

--- part-1/functions.js
+++ part-1/functions.js
@@ -3,7 +3,10 @@
 // weekday(date)
 // returns the weekday for a given date object
 const weekday = (date) => {
-
+  if (!date || typeof date !== 'object') return 'Invalid Date or No Date Object Passed'
+  const dayArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  console.log(date.getDay())
+  return dayArray[date.getDay()]
 }
 
 module.exports = { weekday }
--- part-1/tests.js
+++ part-1/tests.js
@@ -2,9 +2,16 @@
 const expect = require('chai').expect;
 
 describe('weekday', function () {
-  it('should return "Mon" when passed September 11th, 2017', () =>{
     const weekday = require('./functions.js').weekday
-    const date = new Date(2017, 9, 11)
+  it('should return "Mon" when passed September 11th, 2017', () => {
+    const date = new Date(2017, 8, 11)
     expect(weekday(date)).to.equal('Mon')
+  })
+  it('should return "Invalid Date or No Date Object Passed" when a date object isnt passed as an argument', () => {
+    const date = 'cheese'
+    expect(weekday(date)).to.equal('Invalid Date or No Date Object Passed')
+  })
+  it('should return "Invalid Date or No Date Object Passed" when nothing is passed as an argument', () => {
+    expect(weekday()).to.equal('Invalid Date or No Date Object Passed')
   })
 })
