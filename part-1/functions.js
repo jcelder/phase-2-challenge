32d28f3f83255b9686b8d45615c9acb49e8bda4a
Joshua Elder <aresx130@gmail.com>
2017-09-12 19:46:04 -0700

    Added first test of weekday function to tests.js and function stub to functions.js.

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,1 +1,9 @@
 // functions.js
+
+// weekday(date)
+// returns the weekday for a given date object
+const weekday = (date) => {
+
+}
+
+module.exports = { weekday }
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,1 +1,10 @@
 // tests.js
+const expect = require('chai').expect;
+
+describe('weekday', function () {
+  it('should return "Mon" when passed September 11th, 2017', () =>{
+    const weekday = require('./functions.js').weekday
+    const date = new Date(2017, 9, 11)
+    expect(weekday(date)).to.equal('Mon')
+  })
+})
