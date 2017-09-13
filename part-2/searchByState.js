bd31eed0c79e4fffac33a6f85c60af5e378c67c6
Joshua Elder <aresx130@gmail.com>
2017-09-12 22:14:56 -0700

    Added case insensitivity to if statement line 10

--- part-2/searchByState.js
+++ part-2/searchByState.js
@@ -3,12 +3,12 @@
 const fs = require('fs')
 
 const data = JSON.parse(fs.readFileSync('clients.json'))
-const state = process.argv[2]
+const state = process.argv[2].toLowerCase()
 const matchingData = []
-console.log(`Finding clients in state "${state}"`)
+console.log(`Finding clients in state "${process.argv[2]}"`)
 data.forEach((rep) => {
-  if (rep.state === state) matchingData.push({ id: rep.id, rep_name: rep.rep_name, company: rep.company, city: rep.city, state: rep.state })
+  if (rep.state.toLowerCase() === state) matchingData.push({ id: rep.id, rep_name: rep.rep_name, company: rep.company, city: rep.city, state: rep.state })
 })
 
 if (matchingData.length > 0) console.log(matchingData)
-else console.log(`No clients found in state "${state}"`)
+else console.log(`No clients found in state "${process.argv[2]}"`)
